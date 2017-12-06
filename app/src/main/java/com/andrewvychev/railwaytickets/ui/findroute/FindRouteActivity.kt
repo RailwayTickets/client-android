711d1223aae524d5271f0ce962e7d392cc85f00c
Andrew Vychev <andrew.vychev@gmail.com>
2017-12-06 21:57:56 +0300

    Add navigation drawer

--- app/src/main/java/com/andrewvychev/railwaytickets/ui/findroute/FindRouteActivity.kt
+++ app/src/main/java/com/andrewvychev/railwaytickets/ui/findroute/FindRouteActivity.kt
@@ -1,6 +1,10 @@
 package com.andrewvychev.railwaytickets.ui.findroute
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
 import android.view.View
 import android.widget.TextView
 import com.andrewvychev.railwaytickets.R
@@ -14,22 +18,33 @@ import com.andrewvychev.railwaytickets.ui.tickets.TicketsActivity
 import ir.mirrajabi.searchdialog.SimpleSearchDialogCompat
 import ir.mirrajabi.searchdialog.core.SearchResultListener
 import ir.mirrajabi.searchdialog.core.Searchable
-import kotlinx.android.synthetic.main.activity_find_route.et_date
-import kotlinx.android.synthetic.main.activity_find_route.et_from
-import kotlinx.android.synthetic.main.activity_find_route.et_to
-import kotlinx.android.synthetic.main.activity_find_route.progress
+import kotlinx.android.synthetic.main.activity_find_route.drawer_layout
+import kotlinx.android.synthetic.main.activity_find_route.nav_view
+import kotlinx.android.synthetic.main.app_bar_find_route.toolbar
+import kotlinx.android.synthetic.main.content_find_route.et_date
+import kotlinx.android.synthetic.main.content_find_route.et_from
+import kotlinx.android.synthetic.main.content_find_route.et_to
+import kotlinx.android.synthetic.main.content_find_route.progress
 import org.threeten.bp.LocalDate
 import javax.inject.Inject
 
 
 class FindRouteActivity : MvpActivity<FindRouteContract.View>(), FindRouteContract.View,
-        DatePickerFragment.Listener {
+        DatePickerFragment.Listener, NavigationView.OnNavigationItemSelectedListener {
 
     @Inject lateinit var presenter: FindRouteContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_find_route)
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun injectDependencies() {
@@ -40,6 +55,11 @@ class FindRouteActivity : MvpActivity<FindRouteContract.View>(), FindRouteContra
     }
 
     override fun getPresenter(): Contract.Presenter<FindRouteContract.View> = presenter
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 
     fun onNextClicked(view: View) {
         presenter.onNextClicked(et_from.text.toString(),
@@ -64,6 +84,9 @@ class FindRouteActivity : MvpActivity<FindRouteContract.View>(), FindRouteContra
         showSearchDialog(to, {
             et_to.setText(it.title, TextView.BufferType.EDITABLE)
         })
+    }
+
+    override fun onBackPressed() {
     }
 
     fun onChooseDateClicked(view: View) {
